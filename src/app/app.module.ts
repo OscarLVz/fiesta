bfce6806055af42b8767a827633a628df82986a3
OscarLVz <emmanuellv@hotmail.com>
2022-11-15 21:41:34 -0600

    Navbar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/public/home/home.component';
 import { SearchComponent } from './components/public/home/search/search.component';
+import { NavbarComponent } from './components/public/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
