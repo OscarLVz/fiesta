7bbc1da10de94be8373ef4fb31796099faa76bbb
OscarLVz <emmanuellv@hotmail.com>
2022-11-15 21:34:43 -0600

    restructure search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/public/home/home.component';
+import { SearchComponent } from './components/public/home/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
