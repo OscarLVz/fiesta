5aa7c4f4076b43728f4b11d87eefdd55ea3c52f6
OscarLVz <emmanuellv@hotmail.com>
2022-11-16 22:26:26 -0600

    Deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/public/home/home.component';
 import { SearchComponent } from './components/public/home/search/search.component';
 import { NavbarComponent } from './components/public/navbar/navbar.component';
+import { CategoriesComponent } from './components/public/home/categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
